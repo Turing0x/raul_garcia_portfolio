4e7e885dd6c588dd000fe6c00d187616291d1955
Turing0x <rauligf97@gmail.com>
2025-01-04 22:36:51 -0500

    Enhance internationalization and improve component functionality across the application
    - Integrated translation support in Footer, About, ProjectCard, and Projects components to display content in English and Spanish.
    - Updated Footer to utilize translated labels for titles, descriptions, and form placeholders, enhancing user engagement.
    - Refactored About component to dynamically render skills and attributes based on the current language, improving clarity and consistency.
    - Enhanced ProjectCard to conditionally render project descriptions with highlighted keywords, improving readability.
    - Updated Projects component to display project titles and descriptions in the selected language, ensuring a cohesive user experience.
    - Improved ScrollProgressButton functionality by adding language detection and dynamic label updates based on the current locale.

--- src/i18n/labes.ts
+++ src/i18n/labes.ts
@@ -3,24 +3,73 @@ import type { Lang } from "../types/lang";
 export const labels: Record<Lang, Record<string, string>> = {
   en: {
     "document.scroll-btn": "Scroll down",
+    "projects.title": "Projects",
+    "about.title": "About me",
 
     // Header start
 
     "header.role": "Full Stack Web /*&*/ Mobile Developer",
     "header.description":
-      "I'm a /*full stack web and mobile developer*/ with a passion for creating /*innovative and efficient solutions*/. I have a /*strong background*/ in JavaScript, TypeScript, Angular, RxJS, Node.js, Flutter - Dart and MongoDB. I'm always looking for new challenges and /*opportunities to learn and grow.*/",
+      "I'm /*full stack web and mobile developer*/ with a passion for creating /*innovative and efficient solutions*/. I have a /*strong background*/ in JavaScript, TypeScript, Angular, RxJS, Node.js, Flutter - Dart and MongoDB. I'm always looking for new challenges and /*opportunities to learn and grow.*/",
 
     // Header end
+
+    // About start
+    "about.expertise.title": "Core Expertise",
+    "about.key-attributes.title": "Key Professional Attributes",
+    "about.expertise.3": "Tools & Others",
+
+    "about.personal-info.description":
+      "I am /*Computer Science Engineer*/ graduated with a strong academic record and extensive industry experience as a /*senior full-stack developer*/. I specialize in crafting efficient, functional, and visually appealing applications, focusing on /*user experience, interface design, and database optimization*/. My blend of theoretical knowledge and practical expertise allows me to deliver high-quality technological solutions with strategic precision.",
+
+    // About end
+
+    // Footer start
+
+    "footer.title": "Let's work together!",
+    "footer.description":
+      "Do you like my work and want a similar solution for your company? Sure, let's get in touch now!",
+
+    "footer.form.name": "Name",
+    "footer.form.email": "Email",
+    "footer.form.message": "Message",
+    "footer.form.submit": "DROP ME AN EMAIL",
+
+    // Footer end
   },
   es: {
     "document.scroll-btn": "Desplazar hacia abajo",
+    "projects.title": "Proyectos",
+    "about.title": "Sobre mí",
 
     // Header start
 
     "header.role": "Desarrollador Full Stack Web /*&*/ Móvil",
     "header.description":
-      "Soy un /*desarrollador web y móvil full stack*/ con pasión por crear /*soluciones innovadoras y eficientes*/. Tengo una /*sólida formación*/ en JavaScript, TypeScript, Angular, RxJS, Node.js, Flutter - Dart y MongoDB. Siempre estoy buscando nuevos desafíos y /*oportunidades para aprender y crecer.*/",
+      "Soy /*desarrollador web y móvil full stack*/ con pasión por crear /*soluciones innovadoras y eficientes*/. Tengo una /*sólida formación*/ en JavaScript, TypeScript, Angular, RxJS, Node.js, Flutter - Dart y MongoDB. Siempre estoy buscando nuevos desafíos y /*oportunidades para aprender y crecer.*/",
 
     // Header end
+
+    // About start
+
+    "about.expertise.title": "Experiencia Central",
+    "about.key-attributes.title": "Atributos Profesionales Clave",
+    "about.expertise.3": "Herramientas & Otros",
+    "about.personal-info.description":
+      "Soy /*Ingeniero en Ciencias de la Computación*/ graduado con un sólido historial académico y una amplia experiencia en la industria como /*desarrollador senior full-stack*/. Me especializo en la creación de aplicaciones eficientes, funcionales y visualmente atractivas, con foco en la /*experiencia del usuario, el diseño de interfaces y arquitectura de sistemas*/. Mi combinación de conocimientos teóricos y experiencia práctica me permite ofrecer soluciones tecnológicas de alta calidad con precisión estratégica.",
+
+    // About end
+
+    // Footer start
+
+    "footer.title": "¡Trabajemos juntos!",
+    "footer.description":
+      "¿Te gusta mi trabajo y quieres una solución similar para tu empresa? ¡Claro, ¡contactémonos ahora!",
+    "footer.form.name": "Nombre",
+    "footer.form.email": "Email",
+    "footer.form.message": "Mensaje",
+    "footer.form.submit": "ENVÍAME UN EMAIL",
+
+    // Footer end
   },
 };
